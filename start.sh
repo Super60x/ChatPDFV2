9851e5546982189e0a36d624f84314d39a07c94b
Super60x <ejsdebakker@gmail.com>
2025-01-28 20:07:48 +0100

    Fix dependencies and optimize startup

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-streamlit run streamlitui.py --server.port=$PORT --server.address=0.0.0.0 --browser.serverAddress=0.0.0.0
+export STREAMLIT_SERVER_MAX_UPLOAD_SIZE=10
+export STREAMLIT_SERVER_ENABLE_CORS=false
+export STREAMLIT_BROWSER_GATHER_USAGE_STATS=false
+
+streamlit run streamlitui.py --server.port=$PORT --server.address=0.0.0.0 --browser.serverAddress=0.0.0.0 --server.maxUploadSize=10
